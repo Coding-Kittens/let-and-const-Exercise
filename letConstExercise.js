b5a9a121fbc8a613543279d881cfed01911002a9
Rose <rosepowell2001@gmail.com>
2022-05-17 15:22:14 -0400

    first commit

---
+++ letConstExercise.js
@@ -0,0 +1,22 @@
+const PI = 3.14;
+// PI = 42;// can't do this
+
+
+///Quiz///
+
+// What is the difference between var and let?
+////var can be declaired more than once and dose not have a different scope inside blocks like for loops or if statements
+//// let you can only declaired once in the same scope and it has a different scope inside blocks like for loops or if statements
+
+// What is the difference between var and const?
+////same differences as between let and var, except const can not be reasinged
+
+
+// What is the difference between let and const?
+//const can not be reasinged, let can be reasinged
+
+
+
+// What is hoisting?
+// hoisting is when it can use a variable before it has been declaired
+//var lets you do this, let and const do not
